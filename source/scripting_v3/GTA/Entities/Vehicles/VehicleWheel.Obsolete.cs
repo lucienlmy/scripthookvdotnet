84c0cf7d55b86e6fadd20a9b4cfdb139f2b636ad
Jonas <jonasthebrinkmann@gmail.com>
2026-07-06 13:14:18 +0200

    feat(api-v3): add `IsBurst` to `VehicleWheel`

--- source/scripting_v3/GTA/Entities/Vehicles/VehicleWheel.Obsolete.cs
+++ source/scripting_v3/GTA/Entities/Vehicles/VehicleWheel.Obsolete.cs
@@ -11,5 +11,20 @@ namespace GTA
         [Obsolete("Use VehicleWheel.BoneId or VehicleWheel.ScriptIndex instead."),
         EditorBrowsable(EditorBrowsableState.Never)]
         public int Index => (int)ScriptIndex;
+
+        /// <summary>                              
+        /// Sets a value indicating whether this <see cref="VehicleWheel"/> is burst.
+        /// </summary>
+        [Obsolete("Use VehicleWheel.IsBurst instead.")]
+        public bool IsBursted
+        {
+            get
+            {
+                if (!TryGetMemoryAddress(out IntPtr address) || SHVDN.NativeMemory.Vehicle.CWheelTireHealthOffset == 0)
+                    return false;
+
+                return SHVDN.MemDataMarshal.ReadFloat(address + SHVDN.NativeMemory.Vehicle.CWheelTireHealthOffset) <= 0f;
+            }
+        }
     }
 }
